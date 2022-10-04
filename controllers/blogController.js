51690bd8f8c25a5efe536a91f08c1dd939c1a530
Dumspirosperoo <93382641+Dumspirosperoo@users.noreply.github.com>
2022-10-04 21:31:21 +0100

    routers complete

---
+++ controllers/blogController.js
@@ -0,0 +1,11 @@
+//blog_index, blog_details, blog_create_get, blog_create_post, blog_delete
+
+const blog_index = 
+
+const blog_details = 
+
+const blog_create_get = 
+
+const blog_create_post = 
+
+const blog_delete= 
